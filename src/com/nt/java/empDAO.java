db245c80fddaa67846f73e325b99fedc20588f06
dell <dell@user>
2019-04-01 15:32:52 +0530

    initial project creation by TL

---
+++ src/com/nt/java/empDAO.java
@@ -0,0 +1,12 @@
+package com.nt.java;
+
+public class empDAO {
+	public static void main(String[] args) {
+		int no=50;
+		findEmp(no);
+	}
+	public static void findEmp(int no){
+		System.out.println(no+"th emp is raja");
+	}
+
+}
